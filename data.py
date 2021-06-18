638dd3aaf745c0019002471df8bab04f1416792e
liuxinxin <cliuxinxin@gmail.com>
2021-06-18 17:53:53 +0800

    完成resources的列表功能

---
+++ data.py
@@ -0,0 +1,19 @@
+import configparser
+import glob
+
+class Data():
+    def __init__(self):
+        self.conf = configparser.ConfigParser()
+        self.conf.read('config.ini')
+        pass
+
+    def list_resources(self):
+        res_document = 'data/' + self.conf['config']['resources'] + '/*' 
+        resources = glob.glob(res_document)
+        return resources
+
+data = Data()
+print(data.list_resources())
+
+
+
